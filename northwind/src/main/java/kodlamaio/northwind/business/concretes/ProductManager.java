1dc94ecc80caab093368c097d5c67c9912255185
ZEHRA <z.nuralkan@gmail.com>
2021-06-04 01:22:18 +0300

    Northwind
    7.Ders Revize

--- northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
+++ northwind/src/main/java/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -6,6 +6,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kodlamaio.northwind.business.abstracts.ProductService;
+import kodlamaio.northwind.core.utilities.results.DataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import kodlamaio.northwind.core.utilities.results.SuccessResult;
 import kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import kodlamaio.northwind.entities.concretes.Product;
 @Service
@@ -19,10 +22,19 @@ private ProductDao productDao;
 	this.productDao = productDao;
 }
 	@Override
-	public List<Product> getAll() {
+	public DataResult<List<Product>> getAll() {
 		// TODO Auto-generated method stub
-		return this.productDao.findAll();
+		return new SuccessDataResult<List<Product>>(this.productDao.findAll(),"Data Listelendi");
+				
 		//bütün dataları getirir.
 	}
+	@Override
+	public kodlamaio.northwind.core.utilities.results.Result add(Product product) {
+		this.productDao.save(product);	
+		return new SuccessResult("Ürün eklendi");
+
+	}
+	
+	
 
 }
---
+++ northwind/src/main/java/kodlamaio/northwind/core/utilities/results/DataResult.java
@@ -0,0 +1,20 @@
+package kodlamaio.northwind.core.utilities.results;
+
+public class DataResult<T> extends Result{
+//T generic 
+	private T data;
+	public DataResult(T data,boolean success, String message) {
+		super(success, message);
+		//base sınıfın constructorlarını çalıştımaya yarar
+		this.data=data;
+	}
+
+	public DataResult(T data,boolean success) {
+		super(success);
+		//base sınıfın constructorlarını çalıştımaya yarar
+		this.data=data;
+	}
+	public T getData() {
+		return this.data;
+	}
+}
