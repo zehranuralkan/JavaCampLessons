f7fe15da64c9d22bd05af40365df57e5d05b55f4
ZEHRA <z.nuralkan@gmail.com>
2021-06-15 14:06:53 +0300

    8.hafta

--- northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
+++ northwind/src/main/java/kodlamaio/northwind/business/abstracts/ProductService.java
@@ -2,7 +2,7 @@ package kodlamaio.northwind.business.abstracts;
 
 import java.util.List;
 
-
+import org.springframework.data.jpa.repository.Query;
 
 import kodlamaio.northwind.core.utilities.results.DataResult;
 import kodlamaio.northwind.core.utilities.results.Result;
@@ -10,5 +10,24 @@ import kodlamaio.northwind.entities.concretes.Product;
 
 public interface ProductService {
 	DataResult<List<Product>> getAll();
+	DataResult<List<Product>> getAllSorted();
+	DataResult<List<Product>> getAll(int pageNo,int pageSize);
+	
 	Result add(Product product);
+	
+	DataResult<Product> getByProductName(String productName);
+
+	DataResult<Product> getByProductNameAndCategory(String productName,int categoryId);
+	
+	DataResult<List<Product>> getByProductNameOrCategory(String productName,int categoryId);
+	
+	DataResult<List<Product>> getByCategoryIn(List<Integer>categories);
+	
+	DataResult<List<Product>> getByProductNameContains(String productName);
+	
+	DataResult<List<Product>> getByProductNameStartsWith(String productName);
+	
+	@Query("From Product where productName=:productName and Category.categoryId=:categoryId")
+	DataResult<List<Product>>getByNameAndCategory(String productName,int categoryId);
+	
 }
